d0496593c9e0e00fa08dec18456d09aa676e3926
maxwen <max.weninger@gmail.com>
2017-12-20 01:16:29 +0100

    crowdin: add android-8.1
    Change-Id: I09a112286c640fd6882f56ff2ee2e2721c193cf3

--- jenkins_crowdin_download.sh
+++ jenkins_crowdin_download.sh
@@ -3,15 +3,15 @@
 source ~/crowdin_key.sh
 
 export OMNI_CROWDIN_BASE_PATH=/data2/android/omni-80-oneplus3/source
-export OMNI_CROWDIN_BRANCH=android-8.0
-export OMNI_CROWDIN_DEVICE=find7
+export OMNI_CROWDIN_BRANCH=android-8.1
+export OMNI_CROWDIN_DEVICE=oneplus3
 
 cd $OMNI_CROWDIN_BASE_PATH
 
 . ./build/envsetup.sh
-python ./omni_crowdin/crowdin_sync.py --username omnibot --local-download
+#python ./omni_crowdin/crowdin_sync.py --username maxwen --local-download
 brunch $OMNI_CROWDIN_DEVICE
 if [ $? -eq 0 ]; then
-    python ./omni_crowdin/crowdin_sync.py --username omnibot --download
+    python ./omni_crowdin/crowdin_sync.py --username maxwen --download
 fi
 
