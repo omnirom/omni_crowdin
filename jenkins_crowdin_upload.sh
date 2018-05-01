d4f2d5fbcab3fdeb8af2d6b698bc8fb9edc3db00
maxwen <max.weninger@gmail.com>
2018-05-01 22:37:51 +0200

    crowdin: add more device repos
    Change-Id: I14cd1280d7bd427e8573cf2fb45579471339347e

--- jenkins_crowdin_upload.sh
+++ jenkins_crowdin_upload.sh
@@ -7,3 +7,7 @@ export OMNI_CROWDIN_DEVICE=oneplus3
 
 cd $OMNI_CROWDIN_BASE_PATH
 python ./omni_crowdin/crowdin_sync.py --username omnibot --upload-sources
+
+# for new repos initial upload add
+#python ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-sources
+#python ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-translations
