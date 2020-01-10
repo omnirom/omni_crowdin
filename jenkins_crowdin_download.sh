8da6424206d241aadcdb9102f6e677f70c832fdf
maxwen <max.weninger@gmail.com>
2020-01-10 23:13:21 +0100

    omni_crowdin: initial android-11
    Change-Id: I025c75fa662f80b53ee50c920f0dbb9123d228bc

--- jenkins_crowdin_download.sh
+++ jenkins_crowdin_download.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 source ~/crowdin_key.sh
 
-export OMNI_CROWDIN_BASE_PATH=/data2/android/omni-100/source
-export OMNI_CROWDIN_BRANCH=android-10
-export OMNI_CROWDIN_DEVICE=omni_emulator
+export OMNI_CROWDIN_BASE_PATH=/data3/android/android-rpi/source
+export OMNI_CROWDIN_BRANCH=android-11
+export OMNI_CROWDIN_DEVICE=omni_rpi4
 
 cd $OMNI_CROWDIN_BASE_PATH
+python3 ./omni_crowdin/crowdin_sync.py --username maxwen --download
 
+#python3 ./omni_crowdin/crowdin_sync.py --username maxwen --local-download
 #. ./build/envsetup.sh
-python ./omni_crowdin/crowdin_sync.py --username maxwen --local-download
 #brunch $OMNI_CROWDIN_DEVICE
 #if [ $? -eq 0 ]; then
 #    python ./omni_crowdin/crowdin_sync.py --username maxwen --download
