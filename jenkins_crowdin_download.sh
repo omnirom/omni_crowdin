9a6edaaa0f9b39eff1f7436cc60487d2ee3b9a04
maxwen <max.weninger@gmail.com>
2020-05-11 16:49:16 +0200

    crowdin: local config
    Change-Id: Ie4f0e76baec3a3fcc31f8fc1075033aa9c8aa644

--- jenkins_crowdin_download.sh
+++ jenkins_crowdin_download.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 source ~/crowdin_key.sh
 
-export OMNI_CROWDIN_BASE_PATH=/data2/android/omni-100/source
+export OMNI_CROWDIN_BASE_PATH=/data4/android/omni-100/source
 export OMNI_CROWDIN_BRANCH=android-10
 export OMNI_CROWDIN_DEVICE=omni_emulator
 
 cd $OMNI_CROWDIN_BASE_PATH
 
 #. ./build/envsetup.sh
-python ./omni_crowdin/crowdin_sync.py --username maxwen --local-download
+python3 ./omni_crowdin/crowdin_sync.py --username maxwen --download
 #brunch $OMNI_CROWDIN_DEVICE
 #if [ $? -eq 0 ]; then
 #    python ./omni_crowdin/crowdin_sync.py --username maxwen --download
--- jenkins_crowdin_upload.sh
+++ jenkins_crowdin_upload.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 source ~/crowdin_key.sh
 
-export OMNI_CROWDIN_BASE_PATH=/data2/android/omni-100/source
+export OMNI_CROWDIN_BASE_PATH=/data4/android/omni-100/source
 export OMNI_CROWDIN_BRANCH=android-10
 export OMNI_CROWDIN_DEVICE=omni_emulator
 
 cd $OMNI_CROWDIN_BASE_PATH
-#python ./omni_crowdin/crowdin_sync.py --username omnibot --upload-sources
+python3 ./omni_crowdin/crowdin_sync.py --username omnibot --upload-sources
 
 # for new repos initial upload add
-python ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-sources
-python ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-translations
+#python3 ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-sources
+#python3 ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-translations
