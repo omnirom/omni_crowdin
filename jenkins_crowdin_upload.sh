8c0dd340b4d555fb5e8c23acb2ab101b111dd5f6
maxwen <max.weninger@gmail.com>
2021-03-25 18:40:58 +0100

    crowdin: update to API v2
    and using java version of crodwin-cli
    
    Change-Id: I735eb8d26f21a102f89797496cc73557aed266e5

--- jenkins_crowdin_upload.sh
+++ jenkins_crowdin_upload.sh
@@ -6,8 +6,8 @@ export OMNI_CROWDIN_BRANCH=android-11
 export OMNI_CROWDIN_DEVICE=omni_rpi4
 
 cd $OMNI_CROWDIN_BASE_PATH
-python3 ./omni_crowdin/crowdin_sync.py --username omnibot --upload-sources
+#python3 ./omni_crowdin/crowdin_sync.py --username omnibot --upload-sources
 
 # for new repos initial upload add
-#python ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-sources
-#python ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-translations
+python3 ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-sources
+python3 ./omni_crowdin/crowdin_sync.py --username omnibot -c upload.yaml --upload-translations
