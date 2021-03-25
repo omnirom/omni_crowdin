d5d44c1c5c74e533ac389323083c61cb1775af6c
maxwen <max.weninger@gmail.com>
2021-03-25 18:40:58 +0100

    crowdin: update to API v2
    and using java version of crodwin-cli
    
    Change-Id: I735eb8d26f21a102f89797496cc73557aed266e5

--- jenkins_crowdin_download.sh
+++ jenkins_crowdin_download.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 source ~/crowdin_key.sh
 
-export OMNI_CROWDIN_BASE_PATH=/data3/android/android-rpi/source
+export OMNI_CROWDIN_BASE_PATH=`pwd`
 export OMNI_CROWDIN_BRANCH=android-11
-export OMNI_CROWDIN_DEVICE=omni_rpi4
+
+if [ ! -f "./omni_crowdin/crowdin_sync.py" ]; then
+    echo "Must be started in build root folder"
+    exit 0
+fi
 
 cd $OMNI_CROWDIN_BASE_PATH
-python3 ./omni_crowdin/crowdin_sync.py --username maxwen --download
+python3 ./omni_crowdin/crowdin_sync.py --username omnibot --download
 
-#python3 ./omni_crowdin/crowdin_sync.py --username maxwen --local-download
+# with test build for a device before uploading
+#python3 ./omni_crowdin/crowdin_sync.py --username omnibot --local-download
 #. ./build/envsetup.sh
-#brunch $OMNI_CROWDIN_DEVICE
+#brunch <device>
 #if [ $? -eq 0 ]; then
-#    python ./omni_crowdin/crowdin_sync.py --username maxwen --download
+#    python3 ./omni_crowdin/crowdin_sync.py --username omnibot --download
 #fi
 
+exit 1
